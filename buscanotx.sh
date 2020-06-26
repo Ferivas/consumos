d1d159975bfefa7a9e1acb27302df3d9b44a72ae
Ferivas <fvasquez@watching.com.ec>
2020-06-26 18:15:35 -0500

    Archivos bash para buscanotx

---
+++ buscanotx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Optionally set PYTHONPATH
+#export PYTHONPATH=/path/to/python/libraries:$PYTHONPATH
+
+# Run your script with your preferred Python installation
+# in case you have more than one
+cd /home/pi/consumos
+python3 buscanotx.py
